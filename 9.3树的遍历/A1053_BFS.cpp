38e6b4675a7749846766737483a5496d7fda5a76
RisanLi <as5754@163.com>
2018-04-20 20:01:31 +0800

    DFS遍历静态一般树，DFS适用于求出符合条件的所有路径

--- 9.3树的遍历/A1053_BFS.cpp
+++ 9.3树的遍历/A1053_BFS.cpp
@@ -1,12 +1,14 @@
 #include <cstdio>
 #include <vector>
 #include <cstring>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 struct Node{
 	int weight;
 	vector<int> children;
+	int distance;	//路径和
 } node[110];
 vector<vector<int> > paths;
 int N, M, S;
@@ -14,28 +16,19 @@ int N, M, S;
 int sum = 0;
 vector<int> p;
 bool isLeaf[110];
-void DFS(int root){		//根序遍历
-	if (sum > S)	//剪枝
-		return;
-	//访问节点
-	sum += node[root].weight;
-	p.push_back(root);
-	//访问孩子节点
-	for (int i : node[root].children){
-		DFS(i);
-		p.pop_back();	//访问完一个节点要弹出该节点类似二叉树的DFS
-		sum -= node[i].weight;
-	}
-
-	//边界的放置位置要带入例子具体确定
-	if (sum == S && isLeaf[p.back()]){	//边界
-		paths.push_back(p);
-	//	sum -= node[p.back()].weight;
-	//	p.pop_back();
-		return;
+void BFS(int root){
+	node[root].distance = 10;
+	queue<int> q;
+	q.push(root);
+	while(!q.empty()){
+		int temp = q.front();
+		q.pop();
+		for (int i : node[temp].children){
+			node[i].distance = node[i].weight + node[temp].distance;
+			q.push(i);
+		}
 	}
 }
-
 bool cmp(vector<int> a, vector<int> b){
 	int m = min(a.size(),b.size());
 	for (int i = 0; i < m; i++){
@@ -63,16 +56,10 @@ int main(){
 			node[id].children.push_back(c);
 		}
 	}
-	DFS(0);
-	sort(paths.begin(),paths.end(),cmp);
-	for (vector<int> i : paths){
-		int var = 0;
-		for (int j : i){
-			if (var++ != 0)
-				printf (" ");
-			printf ("%d", node[j].weight);
-		}
-		printf ("\n");
+	BFS(0);
+	for (int i = 0; i < N; i++){
+		if (node[i].distance == S && isLeaf[i])
+			printf ("%d\n",i);
 	}
 	return 0;
 }
